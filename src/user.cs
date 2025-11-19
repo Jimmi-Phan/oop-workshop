2940a6a87861343b7049171c641159bd063a01bb
LaDebug <jonas.joergensen05@gmail.com>
2025-11-19 13:20:49 +0100

    User class template

--- src/user.cs
+++ src/user.cs
@@ -1,4 +1,44 @@
-interface IUser()
+//Library
+using System.ComponentModel;
+using System.Net.Http.Headers;
+
+abstract class User
 {
-    
+    private int cpr;
+    private string password;
+    public string name;
+    private int age;
+    private string role;
 }
+
+class Borrower : User
+{
+    List<Media> borrowedMedia = new List<Media>{};
+
+    public Borrower(List<Media> borrowed)
+    {
+        borrowedMedia = borrowed;
+    }
+
+    public List<Media> getLoans()
+    {
+        
+    }
+
+    public void loan()
+    {
+        
+    }
+
+    public void returnMedia()
+    {
+        
+    }
+
+    public List<Media> getMedia()
+    {
+        
+    }
+}
+
+
