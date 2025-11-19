09414dd5e6d54dc33e88c29eb72e21de93a507fe
LaDebug <jonas.joergensen05@gmail.com>
2025-11-19 12:14:06 +0100

    Initial user registration

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string session_user = "";
+
+Console.WriteLine("Welcome to the library. Please enter your username below:");
+
+while (session_user == null)
+{
+    session_user = Console.ReadLine();
+}
+
